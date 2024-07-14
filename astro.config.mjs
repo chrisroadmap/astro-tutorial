b894a6630809a65fb2b65cc62e7a9fb68b32dc4c
Chris Smith <chrisroadmap@gmail.com>
2024-07-14 11:39:36 +0100

    Finished tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://wonderful-quokka-49a3f1.netlify.app"
+  site: "https://wonderful-quokka-49a3f1.netlify.app",
+  integrations: [preact()]
 });
